83d483fcee73f5e10c33659cca2096cb15494b49
johirhasan <johirhasan10203@gmail.com>
2024-05-12 22:30:34 +0600

    '_'

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+// Primitive data-types(call by value)
+// string,number,boolean,null,undefined,symbol(to create variable unique),BigInt.
+
+//Refference type Or non-primitive(by allocate refference)
+//Array,object,Functions
+
+const name = ["jahir", "hasan"];
+let myObj = {
+    name: "jahir",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("non-primitive data-type");
+}
--- test.js
+++ test.js
@@ -1,14 +1,18 @@
-let myName = "Jahir Hasan"
-console.log(myName);
+// let myName = "Jahir Hasan"
+// console.log(myName);
 
 let test1 = 123
-console.log(typeof test1);
+// console.log(typeof test1);
 let test2 = String(test1);
-console.log(typeof (test2));
-console.log(test2);
+// console.log(typeof (test2));
+// console.log(test2);
 
-let test3 = "145ab"
-let test4 =Number(test3)
-console.log(typeof(test4));
-console.log((test4));//if the string include a number convert to Number that pprint NaN(not a number)
-console.log(typeof(null));// Undefine is a undefine type but null is object. 
+// let test3 = "145ab"
+// let test4 =Number(test3)
+// console.log(typeof(test4));
+// console.log((test4));//if the string include a number convert to Number that pprint NaN(not a number)
+// console.log(typeof(null));// Undefine is a undefine type but null is object. 
+
+//== equally check and comparisons like > < >= work differently
+//strict check or equal   ===
+console.log("2" === 2);
