b5c32d9a9418e72b18f46e877f7374f0f9d0ca49
johirhasan <johirhasan10203@gmail.com>
2024-05-16 16:51:37 +0600

    string and its prototypes

---
+++ string.js
@@ -0,0 +1,14 @@
+const name = "jahir"
+const age = 4
+
+console.log(`My name is ${name} and my age is ${age}`);//backtics (a way of cal string )
+console.log(name[0]);
+// console.log(name.toUpperCase());//Many  function available in string prototype 
+// console.log(name.indexOf('h'));  
+const dream = "Gotoaboard"
+const newstring = dream.substring(9 , 4)// cant take neagative value[If the end value is less than start value then the end valu will be start value and the start value is end point]
+console.log(newstring);
+const hotasha = dream.slice(-10 , 10)// negative value accepted
+console.log(hotasha);
+//console.log(newstring.length);
+// console.log(dream.charAt(5));
